4cdaf29a2ad471df0e1f59f165d1fed1ff743399
Krizia <krizialudovico@gmail.com>
2018-06-27 19:23:57 -0700

    uploaded

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// $(".btn").click(function(){
+//     $("#animate").toggleClass("menu-open");
+//     $("#animate").toggleClass("menu-collapsed");
+//     $("#animate ul li").toggleClass("hiden animated fadeInLeft");
+// });
